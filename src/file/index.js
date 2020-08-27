4281ef589d7a0f8cc74e595c12912eb7b6e6e061
ghypnus <ghypnus@163.com>
2020-08-28 00:16:14 +0800

    附件上传

--- src/file/index.js
+++ src/file/index.js
@@ -2,7 +2,7 @@
  * 文件工具
  * @author john.gao
  */
-'use strict';
+import { v4 } from 'uuid';
 
 export default {
   /**
@@ -11,7 +11,7 @@ export default {
    * @param  {String} contentType file type
    * @return {Blob}
    */
-  convertBase64ToFile(base64, contentType) {
+  convertBase64ToFile(base64, contentType = 'image/png') {
     var byteCharacters = atob(base64);
     var byteNumbers = new Array(byteCharacters.length);
     for (var i = 0; i < byteCharacters.length; i++) {
@@ -28,42 +28,19 @@ export default {
    * upload file
    * @param  {Array} list An Array of base64 string
    */
-  upload(params) {
-    var self = this;
-    var url = params.url,
-      token = params.token,
-      deviceid = params.deviceid,
-      list = params.list,
-      paramsJson = params.paramsJson,
-      contentType = params.contentType;
+  upload({ url, token, file, contentType = 'image/png'}) {
     var formData = new FormData();
-    if(token)formData.append('token', token);
-    if(deviceid)formData.append('deviceid', deviceid);
-    if(paramsJson){
-      Object.keys(paramsJson).map(key=>{
-        if(paramsJson[key])formData.append(key,paramsJson[key])
-      })
+    if (token) formData.append('token', token);
+    var realBase64 = file.split(',')[1];
+    var value = this.convertBase64ToFile(realBase64, contentType);
+    var suffix = '.' + contentType.split('/')[1];
+    if (value) {
+      formData.append('file', value, v4() + suffix);
     }
-    var fileList = list.map(function(item, index) {
-      var realBase64 = item.split(',')[1];
-      var name = 'file-' + index;
-      var value = self.convertBase64ToFile(realBase64, contentType)
-      if(value)formData.append(name, value, name + '.png')
-    })
-    return new Promise(function(resolve, reject) {
-      $.ajax({
-        url: url,
-        type: 'POST',
-        data: formData,
-        contentType: false,
-        processData: false,
-        success: function(res) {
-          resolve(typeof res === 'string' ? JSON.parse(res) : res)
-        },
-        error: function(err) {
-          reject(err)
-        }
-      })
+    return axios({
+      url: url,
+      method: 'POST',
+      data: formData
     })
   }
 }
